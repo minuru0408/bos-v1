d92e482bc71acf5cf4e5507f924010ad36b28ea8
Your Name <your@email.com>
2025-05-08 22:43:17 +0900

    port change

--- app.py
+++ app.py
@@ -64,5 +64,5 @@ def speak():
     return jsonify({"url": url})
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5001)
 
